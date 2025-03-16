e310f428dc4e036809bddb2e90a1b6593401e23d
Anchaeyeon <s2328@e-mirim.hs.kr>
2025-03-16 19:05:23 +0900

    폴더 구조 변경

--- app.js
+++ app.js
@@ -1,19 +1,4 @@
 const express = require('express');
-const mysql = require('mysql2');
-const connection = mysql.createConnection({
-  host : 'localhost',
-  user : 'root',
-  password : '111111',
-  database : 'mysql'
-});
+const app = express();
 const port = 3000;
-
-connection.connect();
-
-connection.query('SELECT * FROM node_todo', (error, rows, fields) => {
-  if (error)
-    throw error;
-  console.log('node_todo info is : ', rows);
-});
-
-connection.end();
+const connection = require('./config/config.js');
---
+++ config/config.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host : 'localhost',
+  user : 'root',
+  password : '111111',
+  database : 'mysql' // 엑세스 할 데이터베이스
+});
+
+connection.connect();
+
+connection.connect((err) => {
+  if (err) {
+    console.log('database 연결 실패');
+    return;
+  }
+  console.log('database 연결 성공');
+});
+
+module.exports = connection;
